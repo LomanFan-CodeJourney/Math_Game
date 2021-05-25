abe87f8f65a6ed4913b18e401228e3c175e407a6
Liuming <lfan.canada@gmail.com>
2021-05-25 17:35:13 +0000

    add math game

---
+++ mathGame.rb
@@ -0,0 +1,68 @@
+class Question
+  attr_accessor :generate_question, :answer
+
+  def initialize
+    num1 = rand(1..20)
+    num2 = rand(1..20)
+    @answer = num1 + num2
+    @generate_question = "What is #{num1} + #{num2}?"
+  end
+
+end
+
+class Player 
+  attr_accessor :name, :lives
+
+  def initialize(name)
+    @name = name
+    @lives = 3
+  end
+  
+  def name
+    @name
+  end
+
+  def remove_life
+    @lives -= 1
+  end
+  
+end
+
+
+class Game 
+
+  def initialize
+    @player1 = Player.new("Player1")
+    @player2 = Player.new("Player2")
+    @current_player = @player1
+  end
+  
+  def start
+    while @player1.lives > 0 && @player2.lives > 0
+      question = Question.new
+      puts "------#{@current_player.name}'s turn------"
+      
+      puts "#{@current_player.name}: #{question.generate_question}: "
+      print ">"
+
+      if gets.chomp.to_i != question.answer
+        @current_player.lives -= 1
+        puts "Seriously? No!"
+      else
+        puts "YES! You are correct~!"
+      end
+      
+      if @current_player == @player1
+         @current_player = @player2
+      else
+         @current_player = @player1
+      end
+      puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
+    end
+    puts "-----------GAME OVER-----------"
+    puts "------Thanks for playing!------"
+    puts "#{@current_player.name} wins with a score of #{@current_player.lives}/3" 
+  end
+end
+
+Game.new.start
